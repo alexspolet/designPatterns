755dea8984be1464ca9c4b780c597b7e7e6341e2
alex <lelikpolet@gmail.com>
2018-12-27 21:10:12 +0300

    singleton

--- index.php
+++ index.php
@@ -5,3 +5,18 @@
  * Date: 27.12.18
  * Time: 20:32
  */
+
+use Patterns\Singleton;
+
+function __autoload($name){
+    require str_replace('\\' , DIRECTORY_SEPARATOR , $name) . '.php';
+}
+
+$singleton = Singleton::getInstance();
+$singleton->setValue('name' , 'Ivan');
+$singleton->setValue('surname' , 'Romanov');
+echo $singleton->getValue('surname') . '<br>';
+
+$singleton2 =  Singleton::getInstance();
+echo  $singleton->getValue('name');
+
